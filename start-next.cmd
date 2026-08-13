bbbb9a83016df99620b2714f043dd06168e48800
mirshadt <irshaadcse@gmail.com>
2026-08-13 20:15:22 +0300

    memeory changes

--- start-next.cmd
+++ start-next.cmd
@@ -1,4 +1,5 @@
 @echo off
 cd /d C:\Users\Thinkpad\Documents\SalamLIR
+set "NODE_OPTIONS=--max-old-space-size=4096"
 "C:\Program Files\nodejs\node.exe" node_modules\next\dist\bin\next start --hostname 127.0.0.1 --port 8082 > next-launch.log 2>&1
 echo Next exited with code %ERRORLEVEL% >> next-launch.log
